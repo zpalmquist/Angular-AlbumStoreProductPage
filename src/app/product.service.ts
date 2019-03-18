0bf453da1ed90a5b02d0b54cc80acc02aaa921b0
Zac Palmquist <zachary.palmquist@charter.com>
2019-03-18 14:51:16 -0600

    adds observables, types

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -10,8 +11,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number){
+  getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl).map((response) =>
-    response.json());
+    <Album>response.json());
   }
 }
